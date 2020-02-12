5579683aa4832b820b871e0a06040ce4b04fc1d7
Philippe Renon <philippe.renon@neoxam.com>
2020-02-12 18:35:08 +0100

    better timer example

--- examples/timer.rs
+++ examples/timer.rs
@@ -10,23 +10,37 @@ fn main() {
     simple_logger::init().unwrap();
     let event_loop = EventLoop::new();
 
-    let _window = WindowBuilder::new()
+    let window = WindowBuilder::new()
         .with_title("A fantastic window!")
         .build(&event_loop)
         .unwrap();
 
     let timer_length = Duration::new(1, 0);
 
+    let mut redraw = false;
+
     event_loop.run(move |event, _, control_flow| {
         println!("{:?}", event);
-
         match event {
-            Event::NewEvents(StartCause::Init) => {
-                *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length)
+            Event::NewEvents(start_cause) => {
+                redraw = true;
+                match start_cause {
+                    StartCause::Init => {
+                        *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length)
+                    }
+                    StartCause::ResumeTimeReached { .. } => {
+                        *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length);
+                        println!("\nTimer\n");
+                    }
+                    _ => {
+                        redraw = false;
+                    }
+                }
             }
-            Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
-                *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length);
-                println!("\nTimer\n");
+            Event::MainEventsCleared => {
+                if redraw {
+                    window.request_redraw();
+                }
             }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
